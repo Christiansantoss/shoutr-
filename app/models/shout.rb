e25b03160a6af5ca4a98e39d95d2b001eac52128
Christiansantoss <nismoskyi@yahoo.com>
2020-02-07 11:17:17 -0800

    moved body validations from shout.rb to textshout.rb

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,7 +2,7 @@ class Shout < ApplicationRecord
   belongs_to :user
   belongs_to :content, polymorphic: :true
 
-  validates :body, presence: true, length: { in: 1...44 }
+  
   validates :user, presence: true
 
   default_scope { order(created_at: :desc) }
